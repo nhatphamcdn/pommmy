78e9792a2b6156579a239c29a79e78c9981aaf8a
nhatpham95 <nhat@madisontech.co>
2019-08-06 11:14:47 +0700

    Update

--- index.php
+++ index.php
@@ -8,26 +8,42 @@
 	<link rel="stylesheet" href="./style.css">
 </head>
 <body>
-	<div class="conatiner"><span class="numer">4</span>
-		<div class="circle">
-			<div class="drops"></div>
-			<div class="drops"></div>
-			<div class="hand"></div>
-			<div class="hand rgt"></div>
-			<div class="holder">
-				<div class="bob">
-					<div class="nose"></div>
-					<div class="face">
-						<div class="mouth">
-							<div class="tongue"></div>
-						</div>
-					</div>
-					<div class="ear"></div>
-					<div class="ear rgt"></div>
-					<div class="neck"></div>
-				</div>
-			</div>
-		</div><span class="numer">4</span>
+	<div class="container">
+		<div class="balloon white">
+			<div class="star-red"></div>
+		<div class="face">
+			<div class="eye"></div>
+			<div class="mouth happy"></div>
+		</div>
+		<div class="triangle"></div>
+		<div class="string"></div>
 	</div>
+
+	<div class="balloon red">
+		<div class="star"></div>
+		<div class="face">
+			<div class="eye"></div>
+			<div class="mouth happy"></div>
+		</div>
+		<div class="triangle"></div>
+		<div class="string"></div>
+	</div>
+
+	<div class="balloon blue">
+		<div class="star"></div>
+		<div class="face">
+			<div class="eye"></div>
+			<div class="mouth happy"></div>
+		</div>
+		<div class="triangle"></div>
+		<div class="string"></div>
+	</div>
+		<div id="timer"></div>
+		<h1>Ngày 22 tháng 9</h1>
+	</div>
+	<footer>
+		<p>Clone by <a href="http://nhatphamcdn.herokuapp.com" target="_blank"> NHAT</a> ♡
+	</footer>
+	<script src="./main.js"></script>
 </body>
 </html>
